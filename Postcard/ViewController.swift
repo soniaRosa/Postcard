70e3a4079364f716e8eed8368cdc6d091dc0ab58
Joao Rosa <joaorosa@air-de-joao.lan>
2014-09-15 16:35:11 +0000

    first rev of project

---
+++ Postcard/ViewController.swift
@@ -0,0 +1,42 @@
+//
+//  ViewController.swift
+//  Postcard
+//
+//  Created by Joao Rosa on 13/09/14.
+//  Copyright (c) 2014 Sonia Rosa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    //IBOutlet
+    @IBOutlet weak var messageLabel: UILabel!
+    @IBOutlet weak var enterNameTextField: UITextField!
+    @IBOutlet weak var enterAMessageTextField: UITextField!
+    @IBOutlet weak var mailButton: UIButton!
+    
+    //IBAction
+    @IBAction func sendMailButtonPressed(sender: UIButton) {
+        //code will evaluate when we the press button
+        messageLabel.hidden = false
+        messageLabel.text = enterAMessageTextField.text
+        enterAMessageTextField.text = ""
+        enterAMessageTextField.resignFirstResponder()
+        messageLabel.textColor = UIColor.redColor()
+        mailButton.setTitle("Mail Sent", forState: UIControlState.Normal)
+    }
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
